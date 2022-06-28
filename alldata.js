85b429ef19de7d5e72114a5232d4056920a951cd
keith howard <keithwhoward88@gmail.com>
2022-06-27 22:54:31 -0400

    update alldata

--- alldata.js
+++ alldata.js
@@ -1,8 +1,25 @@
 function AllData() {
-    const ctx = React.useContext(UserContext);
-    return (
-        <h1>All Data
-            <br/>{JSON.stringify(ctx)}
-        </h1>
-    );
+    const currentUserCtx = React.useContext(currentUserContext);
+    if (currentUserCtx.loginStatus) {
+        const font = "15px";
+        const ctx = React.useContext(UserContext);
+        return (
+            <Card
+                bgcolor="info"
+                txtcolor="white"
+                fontSizeHeader="1.5"
+                headerBgColor="#F65058FF"
+                header="All Data"
+                body={
+                    <>
+                        <br/><h1 style={{fontSize: font}}>Name: <p style={{fontSize: font}} className="float-right text-white">{ctx.users[currentUserCtx.index].name}</p></h1>
+                        <br/><h1 style={{fontSize: font}}>Email: <p style={{fontSize: font}} className="float-right text-white">{ctx.users[currentUserCtx.index].email}</p></h1>
+                        <br/><h1 style={{fontSize: font}}>Balance: <p style={{fontSize: font}} className="float-right text-white">{ctx.users[currentUserCtx.index].balance}</p></h1>
+                    </>
+                }
+                />
+        );
+    } else {
+        alert("Please Log in");
+    }
 }
