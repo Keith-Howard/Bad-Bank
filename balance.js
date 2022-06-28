45c6e417b07b452a22d98a9e8f8cc08f9792159f
Keith Howard <54951171+Keith-Howard@users.noreply.github.com>
2022-06-27 21:40:35 -0400

    Add balance

--- balance.js
+++ balance.js
@@ -1,8 +1,28 @@
 function Balance() {
-    const ctx = React.useContext(UserContext);
-    return (
-        <h1>Balance
-            <br/>{JSON.stringify(ctx)}
-        </h1>
-    );
+    const currentUserCtx = React.useContext(currentUserContext);
+    if (currentUserCtx.loginStatus) {
+        const ctx = React.useContext(UserContext);
+        function getBalance() {
+            document.getElementById('balance').value = ctx.users[currentUserCtx.index].balance;
+        }
+        return (
+            <Card
+                bgcolor="info"
+                txtcolor="white"
+                fontSizeHeader="1.5"
+                headerBgColor="#F65058FF"
+                header="Balance"
+                body={
+                    <>
+                        <br/>Balance<input type="input" readOnly value={ctx.users[currentUserCtx.index].balance} id="balance"
+                        style={{maxWidth: "100px"}} className="form-control float-right bg-info border-info text-white" />
+                        <br/>
+                        <br/><button type="submit" className="btn btn-light" onClick={getBalance}>Deposit</button>
+                    </>
+                }
+                />
+        );
+    } else {
+        alert("Please Log in");
+    }
 }
